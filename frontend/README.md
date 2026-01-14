d0e9c5e31f7210451b6a7d86930f0c4189eb28bc
Kwon Lok Young <kwonlokyoung@gmail.com>
2026-01-14 09:10:30 -0500

    initial commit

---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+# COSIGN
+
+Learn more about cosign [here](https://docs.google.com/presentation/d/13XINdTdAajkJT_2PJTwgp8U28QaNMAk4N8XbP5LtbdE/edit?usp=sharing).
+
+pyenv install 3.10.14
+cd /Signly/backend
+pyenv local 3.10.14
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import CameraStream from './components/CameraStream.js';
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Tophalf from "./components/TopHalf";
+import Hint from "./components/Hint";
+import ImageComponent from "./components/hintImage.js";
+import HandTrackingWebcam from './components/HandTrackingVideo.jsx';
+
+function App() {
+  const [showImage, setShowImage] = useState(false);
+
+  const toggleImage = () => {
+    setShowImage((prevShowImage) => !prevShowImage);
+  };
+  return (
+    <div className="App">
+      <Navbar />
+      <div>
+        <CameraStream />
+        <Hint toggleImage={toggleImage} />
+      </div>
+
+      <ImageComponent show={showImage} />
+      <HandTrackingWebcam />
+      
+    </div>
+  );
+}
+
+export default App;
